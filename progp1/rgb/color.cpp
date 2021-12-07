86fe82f5d11ed3b5890d7c97f05d4984e8fa106e
Pedro Veloso <pedroveloso@gmail.com>
2021-12-07 10:02:14 +0000

    Added Constructures for Color Class

--- progp1/rgb/color.cpp
+++ progp1/rgb/color.cpp
@@ -7,47 +7,62 @@ namespace rgb {
     const color color::GREEN(0,255,0);
     const color color::BLUE(0,0,255);
     color::color() {
-
+        r = g = b = 0;
     }
-    color::color(const color& other) {
 
-    }
     color::color(rgb_value red, rgb_value green, rgb_value blue) {
-
+        r = red;
+        g = green;
+        b = blue;
     }
+
+    color::color(const color& other) : color(other.r, other.g, other.b){
+    }
+
     rgb_value color::red() const {
         return r;
     }
+
     rgb_value color::green() const {
         return g;
     }
+
     rgb_value color::blue() const {
         return b;
     }
+
     rgb_value& color::red()  {
         return r;
     }
+
     rgb_value& color::green()  {
         return g;
     }
+
     rgb_value& color::blue()  {
         return b;
     }
+
     color& color::operator=(const color& c) {
         return *this;
     }
+
     bool color::operator==(const color &c) const {
         return false;
     }
+
     bool color::operator!=(const color &c) const {
         return false;
     }
+
     void color::invert() {
 
     }
+
     void color::mix(const color& c, int f) {
 
     }
+
     void color::to_gray_scale() {
 
     }
